198367ac9a842b95c63c5e7a1560d3a3435131db
nilton.seixas <nfsseixas@gmail.com>
2023-04-14 09:34:11 -0300

    feat: adding database.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'AB',
-        'title': 'Blog post 1 blastoise',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'BA',
-        'title': 'Blog post 2 blastoise',
-        'date_posted': 'August 27, 2018'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
